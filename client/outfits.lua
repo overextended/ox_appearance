272344df54e346b12bafaf5f6458ca5e676c80a5
Alexert97 <38686550+Alexert97@users.noreply.github.com>
2022-05-28 12:21:58 +0200

    Update outfits.lua
    Context menu integration.

--- client/outfits.lua
+++ client/outfits.lua
@@ -64,3 +64,134 @@ RegisterCommand('outfit', function(source, args, raw)
 		exports['fivem-appearance']:setPlayerAppearance(appearance)
 	end
 end)
+
+RegisterCommand('wardrobe', function(source, args, raw)
+    lib.registerContext({
+        id = 'save_change',
+        title = 'Outfit Menu',
+        options = {
+        {
+            title = "Save",
+            description = "save new outfit",
+            arrow = true,
+            event = 'ox_appearance:save',
+            args = {slot= "new", name= ""}
+        },
+        {
+            title = "Outfits",
+            event = 'ox_appearance:wardrobe',
+        }}
+    })
+    lib.showContext('save_change')
+end)
+
+RegisterNetEvent('ox_appearance:wardrobe', function()
+    if not outfitNames then
+        getOutfitNames()
+    end
+    options = {}
+    for k, v in pairs(outfitNames) do
+        options[v] = {}
+        options[v].event = 'ox_appearance:setOutfit'
+        options[v].args = {slot = k, name = v}
+    end
+    
+    lib.registerContext({
+        id = 'wardrobe_menu',
+        menu = 'save_change',
+        title = 'Guardaroba',
+        options = options
+    })
+
+    lib.showContext('wardrobe_menu')
+end)
+
+function getTableSize(t)
+    local count = 0
+    for _, __ in pairs(t) do
+        count = count + 1
+    end
+    return count
+end
+
+RegisterNetEvent('ox_appearance:setOutfit', function(data)
+	lib.registerContext({
+		id = 'replace_use',
+		title = 'Replace',
+		menu = 'wardrobe_menu',
+		options = {
+			['Use'] = {
+				event = 'ox_appearance:use',
+				args = data
+				
+			},
+			['Replace'] = {
+				description =  "Replace outfit: "..data.name,
+				event = 'ox_appearance:save',
+				args = data
+				
+			}
+		}
+	})
+	lib.showContext('replace_use')
+
+end)
+
+RegisterNetEvent('ox_appearance:save', function(data)
+	if not outfitNames then
+		getOutfitNames()
+	end
+	if data.slot == "new" then
+		data.slot = getTableSize(outfitNames) + 1
+		local name = lib.inputDialog('New name', {'Insert Name'})
+		if name then
+			local appearance = exports['fivem-appearance']:getPedAppearance()
+			outfitNames[data.slot] = name[1]
+			outfits[data.slot] = appearance
+
+			TriggerServerEvent('ox_appearance:saveOutfit', appearance, data.slot, outfitNames)
+		end
+	else
+		local input = lib.inputDialog('Confirm saving', {'Insert 1234'})
+
+		if input then
+			local pin = tonumber(input[1])
+			if pin == 1234 then
+				Wait(200)
+				local name = lib.inputDialog('New name', {'Rename'})
+				if name then
+					local appearance = exports['fivem-appearance']:getPedAppearance()
+					outfitNames[data.slot] = name[1]
+					outfits[data.slot] = appearance
+			
+					TriggerServerEvent('ox_appearance:saveOutfit', appearance, data.slot, outfitNames)
+				end
+			end
+		end
+	end
+end)
+
+RegisterNetEvent('ox_appearance:use', function(data)
+    local slot = data.slot
+    local appearance = getOutfit(slot)
+
+    if not appearance.model then appearance.model = 'mp_m_freemode_01' end
+    if lib.progressCircle({
+        duration = 3000,
+        position = 'bottom',
+        useWhileDead = false,
+        canCancel = true,
+        disable = {
+            car = true,
+        },
+        anim = {
+            dict = 'missmic4',
+            clip = 'michael_tux_fidget' 
+        },
+    }) then 
+		exports['fivem-appearance']:setPlayerAppearance(appearance)
+    	TriggerServerEvent('esx_skin:save', appearance)
+    else 
+        print('Do stuff when cancelled') 
+    end    
+end)
