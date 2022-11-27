d5fed2a31bbf83a603c2041c985c34926d77715b
Linden <65407488+thelindat@users.noreply.github.com>
2022-11-28 01:27:33 +1100

    feat(client/esx): add skinchanger:getSkin event
    Who even knows if this will work like people expect it to.

--- client/esx.lua
+++ client/esx.lua
@@ -1,8 +1,9 @@
 if GetResourceState('es_extended'):find('start') then
 	ESX = true
+	local exp = exports['fivem-appearance']
 
 	AddEventHandler('skinchanger:loadDefaultModel', function(male, cb)
-		exports['fivem-appearance']:setPlayerModel(male and 'mp_m_freemode_01' or 'mp_f_freemode_01')
+		exp:setPlayerModel(male and 'mp_m_freemode_01' or 'mp_f_freemode_01')
 		if cb then cb() end
 	end)
 
@@ -10,13 +11,13 @@ if GetResourceState('es_extended'):find('start') then
         if not skin then skin = {} end
 		if not skin.model then skin.model = 'mp_m_freemode_01' end
 
-		exports['fivem-appearance']:setPlayerAppearance(skin)
+		exp:setPlayerAppearance(skin)
 
 		if cb then cb() end
 	end)
 
 	RegisterNetEvent('esx_skin:openSaveableMenu', function(submitCb, cancelCb)
-		exports['fivem-appearance']:startPlayerCustomization(function (appearance)
+		exp:startPlayerCustomization(function (appearance)
 			if (appearance) then
 				TriggerServerEvent('esx_skin:save', appearance)
 				if submitCb then submitCb() end
@@ -32,5 +33,9 @@ if GetResourceState('es_extended'):find('start') then
 			props = true,
 			tattoos = true
 		})
+	end)
+
+	AddEventHandler('skinchanger:getSkin', function(cb)
+		cb(exp:getPedAppearance(cache.ped))
 	end)
 end
